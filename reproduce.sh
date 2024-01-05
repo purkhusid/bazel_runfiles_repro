2e6e588f0419c10aa88155bf32875af9b7440884
Daníel P. Purkhús <purkhusid@gmail.com>
2024-01-05 12:31:35 +0000

    Remove flag

--- reproduce.sh
+++ reproduce.sh
@@ -2,24 +2,22 @@
 
 # This works fine
 bazel clean
-bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --nobuild_runfile_links --remote_download_outputs=minimal
+bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --remote_download_outputs=minimal
 
 # This does not work if the build action is cached
 bazel clean
-bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --nobuild_runfile_links --remote_download_outputs=minimal
-bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --nobuild_runfile_links --remote_download_outputs=minimal
+bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --remote_download_outputs=minimal
+bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --remote_download_outputs=minimal
 
 # If we remove --noexperimental_check_output_files this works when the build action is cached and not cached
 bazel clean
-bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --nobuild_runfile_links --remote_download_outputs=minimal
-bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --nobuild_runfile_links --remote_download_outputs=minimal
+bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --remote_download_outputs=minimal
+bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --remote_download_outputs=minimal
 
 # If we remove --remote_download_outputs=minimal this works when the build action is cached and not cached
 bazel clean
-bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --nobuild_runfile_links
-bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files --nobuild_runfile_links
-
+bazel build //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files bazel run //:uploader --disk_cache=~/.cache/bazel-disk-cache --noexperimental_check_output_files 
 # If we remove --disk_cache this works
 bazel clean
-bazel build //:uploader --noexperimental_check_output_files --nobuild_runfile_links --remote_download_outputs=minimal
-bazel run //:uploader --noexperimental_check_output_files --nobuild_runfile_links --remote_download_outputs=minimal
+bazel build //:uploader --noexperimental_check_output_files --remote_download_outputs=minimal
+bazel run //:uploader --noexperimental_check_output_files --remote_download_outputs=minimal
